0ae77c807d84d52946015f03048e3d98f1c4687f
Arkajyoti Basak <arkajbasak1211@gmail.com>
2023-03-23 20:48:27 +0530

    Fixed bug in main

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import filter
 
 input_sentence = "When Prime Minister Scott Morrison publicly lambasted US car giant General Motors after it announced it was pulling the Holden brand from the Australian market, it set the tone for an ugly brawl."
 
-x = linguistics.main(input_sentence)
+x = parser.main(input_sentence)
 y = testT5.main(x)
-z = find_similarity.main(x, y)
+z = filter.main(x, y)
